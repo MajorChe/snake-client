a60070c6d6bad81dfbfda53bea77999c457c1466
charit nanduri <charithnanduri03@gmail.com>
2021-11-03 18:51:28 +0000

    add client.js and refactor using export

--- play.js
+++ play.js
@@ -1,51 +1,6 @@
-const net = require("net");
-const PORT = 9898;
+const {connect} = require('./client');
 
-const configObj = {
-  port: 50541,
-  host: '165.227.47.243'
-};
+console.log("Connecting ...");
+connect();
 
-const client = net.createConnection(configObj);
-
-client.setEncoding('utf8');
-
-// const onTyping = function(message){
-//   //  try {
-//   client.write(message);
-//   // } catch (err){
-//   //   console.log('cannot write to an ended connection');
-//   // }
-// }
-
-// this fires when stuff is typed into the terminal that launched this 'process'
-//  process.stdin.on('data', onTyping); 
-
-client.on("connect", function () {
-  console.log("client is connected to the server.");
-});
-
-client.on("end", function(){
-  console.log('client disconnected from server.');
-});
-
-
-
-// const net = require("net");
-
-// // establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: '165.227.47.243',// IP address here,
-//     port: 50541// PORT number here,
-//   });
-
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-//   return conn;
-// };
-
-// console.log("Connecting ...");
-// connect();
 
