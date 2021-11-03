ddabe0e13018ae77f773cd29e9c7a4a4eea2a247
charit nanduri <charithnanduri03@gmail.com>
2021-11-03 23:25:51 +0000

    add data to the server using write for keys w,a,s,d

--- input.js
+++ input.js
@@ -1,4 +1,6 @@
-const setupInput = function () {
+let connection;
+const setupInput = function (inputValue) {
+  connection = inputValue;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -6,10 +8,18 @@ const setupInput = function () {
   const handleUserInput = function (key) {
     if (key === '\u0003') {
       process.exit();
+    } else if (key === 'w') {
+      connection.write('Move: up');
+    } else if (key === 'a') {
+      connection.write('Move: left');
+    } else if (key === 's') {
+      connection.write('Move: down');
+    } else if (key === 'd') {
+      connection.write('Move: right');
     }
   };
   stdin.on("data", handleUserInput);
   return stdin;
 };
 
-module.exports = {setupInput};
+module.exports = setupInput;
